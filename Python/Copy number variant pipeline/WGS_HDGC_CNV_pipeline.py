e18233e4e4c381fdcb8a38e92e3f1ab12910c6a6
Victoria Fuller <166982794+victoria-fuller@users.noreply.github.com>
2025-11-27 17:07:42 +0000

    Add files via upload

---
+++ Python/Copy number variant pipeline/WGS_HDGC_CNV_pipeline.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python3
+"""
+Script Name: WGS_HDGC_CNV_pipeline.py
+Author: Victoria Fuller
+Date Created: 26/11/2025
+Last Modified 26/11/2025
+
+Description: 
+Retreives file path from output file to automate filtering pipeline with optional scrips
+
+Usage:
+python3 <WGS_HDGC_CNV_pipeline.py> --pipeline <pipeline> --vcf_input <vcf_file> --gene_list <gene_list> --biomart_file <biomart_file> --flank_bp <flank_bp>
+
+Arguments:
+-p --pipeline : Name of the pipeline to be used
+-v --vcf_input : Path to the input vcf file(s)
+-g --gene_list : Path to the gene list file
+-b --biomart_file : Path to the Biomart annotation file 
+-f --flank_bp : Number of flanking base pairs for filtering (integer)
+
+Dependencies: 
+bcftools
+
+Notes:
+Ensure all files exist and paths are correct before running the script
+Compatible with Python 3.x
+
+"""
+
+__author__ = "Victoria Fuller"
+__version__ = "1.0"
+__date__ = "26/11/2025"
+__status__ = "Development"
+
+import argparse
+import sys
+from pathlib import Path
+from datetime import datetime
+import time
+import logging
+import os
+import subprocess
+import re
+
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(
+        description="Automates WGS HDGC CNV filtering pipeline"
+        )
+    parser.add_argument("-p", "--pipeline", required=True, help="Name of the pipeline to be used")
+    parser.add_argument("-v", "--vcf_input", required=False, help="Optional. Path to the annotated input vcf file(s) to process. If not provided, pipeline will combine all annotated VCFs")
+    parser.add_argument("-g", "--gene_list", required=True, help="Path to the gene list file")
+    parser.add_argument("-b", "--biomart_file", required=True, help="Path to the Biomart annotation file")
+    parser.add_argument("-f", "--flank_bp", type=int, default=5000, help="Number of flanking base pairs for filtering (integer)")
+    return parser.parse_args()
+
+def main():
+
+    # Parse command line arguments
+    args = parse_args()    
+
+    # Define command line arguments as variables
+    script_name = Path(sys.argv[0]).stem
+    input_dir = Path("/home/jovyan/session_data/mounted-data-readonly/")
+    output_dir = Path("/home/jovyan/session_data/output_data/")
+
+    if args.vcf_input:
+        logging.info(f"Using input VCF file: {args.vcf_input}")
+        current_file = args.vcf_input
+        args.multiple_vcfs = None
+    else:
+        detected = list(Path(input_dir).glob("*.cnvcall.vcf*"))
+        if not detected:
+            logging.error(f"No VCFs found in {input_dir}")
+            sys.exit(1)
+
+        logging.info(f"No input VCF provided - combining all annotated VCFs")
+
+        args.multiple_vcfs = [str(file) for file in detected]
+        current_file = args.multiple_vcfs[0]
+
+    # Set up logging
+
+    # Set timestamp and start time
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    start_time = time.time()
+
+    # Create log directory if it doesnt exist
+    log_dir = output_dir / "logs"
+
+    if not log_dir.is_dir():
+        log_dir.mkdir(parents=True, exist_ok=True)
+
+    # Create log file
+    log_file = log_dir / f"{script_name}.log"
+
+    logging.basicConfig(
+        level = logging.INFO, 
+        format = "%(asctime)s [%(levelname)s] %(message)s", 
+        handlers = [
+            logging.FileHandler(log_file, mode ="a"), # Append to existing log file
+            logging.StreamHandler(sys.stdout) # Echo to console
+        ]
+    )
+
+    logging.info("="*80)
+    logging.info(f"Created log directory '{log_dir.stem}' if it did not exist.")
+    logging.info(f"Created log file '{log_file.name}' if it did not exist.")
+    logging.info(f"Selected pipeline: {args.pipeline}")
+    logging.info(f"Script '{script_name}' started at {timestamp}")
+    logging.info(f"Input VCF file: {current_file}")
+    logging.info(f"Input directory: {input_dir}")
+    logging.info(f"Output directory: {output_dir}")
+    logging.info(f"Gene list file: {args.gene_list}")
+    logging.info(f"Biomart file: {args.biomart_file}")
+    logging.info(f"Flanking base pairs: {args.flank_bp}")
+    logging.info("# Processing files...")
+    logging.info("="*80)
+
+    # Check input and output directories exist
+    if not input_dir.is_dir():
+        logging.error(f"Error: Input directory '{input_dir}' does not exist.")
+        sys.exit(1)
+
+    if not output_dir.is_dir():
+        logging.info(f"Creating output directory '{output_dir.stem}' ...")
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Define pipelines
+    PIPELINES = {
+        "whole_genome_deletion": [
+            "combine_cnv_vcf.py", 
+            "cnv_annotate.py", 
+            "cnv_filter_deletion.py", 
+            "vcf_to_tsv.py"
+            ],
+        "genes_deletion": [
+            "combine_cnv_vcf.py", 
+            "cnv_annotate.py", 
+            "filter_genes.py",
+            "cnv_filter_deletion.py", 
+            "vcf_to_tsv.py"
+            ],
+    }
+
+    logging.info(f"Selected pipeline '{args.pipeline}' scripts: {PIPELINES[args.pipeline]}")
+
+    def run_script(script, current_file, extra_args=None):
+        """Run a script and return the output file produced. """
+        extra_args = extra_args or []
+
+        # Decide which arguments to pass based on script name
+        if "filter_genes" in script:
+            # Scripts that require all args
+            standard_args = [
+                "--vcf_input", current_file,
+                "--gene_list", args.gene_list,
+                "--biomart_file", args.biomart_file,
+                "--flank_bp", str(args.flank_bp)
+            ]
+        elif "cnv_annotate" in script:
+            standard_args = [
+                "--vcf_input", current_file,
+                "--biomart_file", args.biomart_file,
+                "--flank_bp", str(args.flank_bp)    
+            ]
+            # Accept all input VCFs for first script or none if none provided
+        elif "combine_cnv_vcf" in script:
+            vcfs = getattr(args, "multiple_vcfs", []) or []
+            standard_args = sum([["--vcf_input", file] for file in vcfs], [])
+            current_file = None
+        else:
+            # Scripts that only need the VCF input
+            standard_args = ["--vcf_input", current_file]
+
+        logging.info(f"Running script: {script}")
+    
+        if script.endswith(".py"):
+            cmd = ["python3", script] + standard_args + extra_args
+            logging.info(f"Running: {' '.join(cmd)}")
+        else:
+            raise ValueError(f"Unknown script type: {script}")
+
+        # Run the command and capture stdout
+        output = subprocess.run(cmd, capture_output=True, text=True)
+        logging.info(output.stdout) 
+
+        if output.returncode != 0:
+            logging.error(f"Error: {script} failed with return code {output.returncode}")
+            sys.exit(output.returncode)
+
+        # Extract lines with "written to ..."
+        matches = re.findall(r"written to (.*)", output.stdout)
+        output_file = None
+
+        # Prefer VCF files if multiple outputs are found
+        if matches:
+            vcf_matches = [f for f in matches if re.search(r"\.vcf(\.gz)?$", f)]
+            tsv_matches = [f for f in matches if re.search(r"\.tsv$", f)]
+            if vcf_matches:
+                output_file = vcf_matches[-1] 
+            elif tsv_matches:
+                output_file = tsv_matches[-1]
+            else:
+                logging.warning(f"No VCF output detected from {script}, skipping this script's output")
+                output_file = current_file
+
+        if not output_file:
+            logging.error(f"Error: Could not detect output file from {script}")
+            sys.exit(1)
+
+        logging.info(f"{script} completed successfully, output: {output_file}")
+        return output_file
+
+        
+    def run_pipeline(name):
+        if name not in PIPELINES:
+            raise ValueError(f"Pipeline '{name}' not recognized. Available pipelines: {list(PIPELINES.keys())}")
+        
+        current_file = args.vcf_input
+
+        for script in PIPELINES[name]:
+            script_name = Path(script).stem
+            logging.info(f"Running script: {script_name} on file: {current_file}")
+            current_file = run_script(script, current_file)
+            
+        # Update current_file to the output of the last script
+        logging.info(f"Pipeline '{name}' completed. Final output file: {current_file}")
+
+    # Run the pipeline
+    run_pipeline(args.pipeline)
+
+    duration = time.time() - start_time
+    logging.info(f"Pipeline '{args.pipeline}' completed in {duration:.2f} seconds.")
+
+            
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        # traceback.print_exc()
+        sys.exit(1)
---
+++ Python/Copy number variant pipeline/filter_genes.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+"""
+Script Name: filter_genes.py
+Author: Victoria Fuller
+Date Created: 20/11/2025
+Last Modified 20/11/2025
+
+Description: 
+Description: Keeps variants with in or near to genes of interest
+Cleans BioMart TSV file
+Creates BED file for genes of interest using BioMart file
+Filters VCF file by BED file
+
+Usage:
+python3 <filter_genes> --vcf_input <vcf_file> 
+
+Arguments:
+--vcf_input : Path to the input vcf file(s)
+
+Dependencies: 
+bcftools
+
+Notes:
+Ensure all files exist and paths are correct before running the script
+Compatible with Python 3.x
+
+Notes to user:
+User will need to modify the accompanying gene list file (gene_list.txt) to select required genes of interest
+
+"""
+
+import argparse
+import sys
+from pathlib import Path
+from datetime import datetime
+import time
+import logging
+import os
+import subprocess
+import re
+
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser(
+        description="Filters variants in or near to genes of interest"
+        )
+    parser.add_argument("-v", "--vcf_input", required=True, help="Path to the annotated input vcf file(s) to process")
+
+    # Accept unused arguments so pipeline doesn't break
+    parser.add_argument("-g", "--gene_list", required=True, help="Path to the gene list file")
+    parser.add_argument("-b", "--biomart_file", required=True, help="Path to the Biomart annotation file")
+    parser.add_argument("-f", "--flank_bp", type=int, default=5000, help="Number of flanking base pairs for filtering (integer)")
+    return parser.parse_args()
+
+def main():
+
+    # Parse command line arguments
+    args = parse_args()
+
+    # Define command line arguments as variables 
+    script_name = Path(sys.argv[0]).stem
+    input_dir = Path("/home/jovyan/session_data/mounted-data-readonly/")
+    output_dir = Path("/home/jovyan/session_data/output_data/")
+    vcf_input = (args.vcf_input)
+    gene_list = (args.gene_list)
+    biomart_file = (args.biomart_file)
+    flank_bp = int(args.flank_bp)
+
+
+    # Set up logging
+
+    # Set timestamp and start time
+    timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    start_time = time.time()
+
+    # Create log directory if it doesnt exist
+    log_dir = output_dir / "logs"
+
+    if not log_dir.is_dir():
+        log_dir.mkdir(parents=True, exist_ok=True)
+
+    # Create log file
+    log_file = log_dir / f"{script_name}.log"
+
+    logging.basicConfig(
+        level = logging.INFO, 
+        format = "%(asctime)s [%(levelname)s] %(message)s", 
+        handlers = [
+            logging.FileHandler(log_file, mode ="a"), # Append to existing log file
+            logging.StreamHandler(sys.stdout) # Echo to console
+        ]
+    )
+
+    logging.info("="*80)
+    logging.info(f"Created log directory '{log_dir.stem}' if it did not exist.")
+    logging.info(f"Created log file '{log_file.name}' if it did not exist.")
+    logging.info(f"Script '{script_name}' started at {timestamp}")
+    logging.info(f"Input VCF file: {vcf_input}")
+    logging.info(f"Input directory: {input_dir}")
+    logging.info(f"Output directory: {output_dir}")
+    logging.info("# Processing files...")
+    logging.info("="*80)
+
+    # Check input and output directories exist
+    if not input_dir.is_dir():
+        logging.error(f"Error: Input directory '{input_dir}' does not exist.")
+        sys.exit(1)
+
+    if not output_dir.is_dir():
+        logging.info(f"Creating output directory '{output_dir.stem}' ...")
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    # Clean BioMart file to ensure it has correct tabs
+    def clean_biomart_tsv(biomart_file):
+        """Cleans the BioMart TSV file to make it suitable for filtering"""
+        biomart_file_path = Path(biomart_file)
+        cleaned_tsv = output_dir / (f"{biomart_file_path.stem}.cleaned.tsv")
+        logging.info(f"Cleaning BioMart TSV: {biomart_file} -> {cleaned_tsv}")
+        with open(biomart_file, "r") as infile, open(cleaned_tsv, "w") as outfile:
+            for line in infile:
+                line = line.rstrip("\n")
+                parts = re.split(r"\t+", line)
+                outfile.write("\t".join(parts) + "\n")
+                #outfile.write("\t".join(line.strip().split()) + "\n")
+        return cleaned_tsv
+    
+    biomart_file_cleaned = clean_biomart_tsv(biomart_file)
+
+    if not biomart_file_cleaned:
+        logging.error(f"Error: Could not clean BioMart file")
+        sys.exit(1)
+
+    def build_bed_file(biomart_file, gene_list, flank_bp):
+        """Builds the BED file using the coordinates from the BioMart TSV file for genes of interest"""
+        gene_list_path = Path(gene_list)
+        bed_file = output_dir / f"{gene_list_path.stem}.bed"
+        logging.info(f"Building BED file for genes in {gene_list}")
+
+        with open(gene_list) as gene_file:
+            genes = {line.strip() for line in gene_file if line.strip()}
+            logging.info(f"Genes found: {', '.join(genes)}")
+
+        count_written = 0
+        
+        with open(biomart_file) as biomart, open(bed_file, "w") as output_bed:
+            for i, line in enumerate(biomart):
+                if i == 0:
+                    continue
+                parts = line.rstrip("\n").split("\t")
+
+                if len(parts) < 5:
+                    logging.debug(f"Skipping malformed line {i+1}")
+                    continue
+                chrom, start, end, gene_name = parts[0], int(parts[1]), int(parts[2]), parts[4]
+                if gene_name not in genes:
+                    continue
+                start = max(0, start - flank_bp)
+                end = end + flank_bp
+                
+                chrom = chrom if chrom.startswith("chr") else (f"chr{chrom}")
+                if chrom[3:] in [str(n) for n in range(1, 23)] + ["X", "Y", "M"]:
+                    output_bed.write(f"{chrom}\t{start}\t{end}\t{gene_name}\n")
+                    count_written += 1
+        logging.info(f"{count_written} regions written to {bed_file}")
+
+        if count_written == 0:
+            logging.error(f"No regions written to BED file")
+            sys.exit(1)
+
+        return bed_file
+    
+    bed_file = build_bed_file(biomart_file_cleaned, gene_list, flank_bp)
+    
+    if not bed_file:
+        logging.error(f"Error: Could not build BED file")
+        sys.exit(1)
+
+    def sort_compress_bed(bed_file):
+        """Sorts and compresses the BED file"""
+        sorted_bed = output_dir / f"{bed_file.stem}.sorted.bed"
+        compressed_bed = output_dir / f"{bed_file.stem}.sorted.bed.gz"
+
+        logging.info(f"Sorting BED file: {bed_file} -> {sorted_bed}")
+        subprocess.run(["sort", "-k1,1", "-k2,2n", str(bed_file)], stdout=open(sorted_bed, "w"), check=True)
+    
+        logging.info(f"Compressing BED file: {sorted_bed} -> {compressed_bed}")
+        subprocess.run(["bgzip", "-c", str(sorted_bed)], stdout=open(compressed_bed, "wb"), check=True)
+
+        logging.info(f"Indexing BED file: {compressed_bed}")
+        subprocess.run(["tabix", "-p", "bed", str(compressed_bed)], check=True)
+    
+        return compressed_bed
+    
+    compressed_bed = sort_compress_bed(bed_file)
+
+    if not compressed_bed:
+        logging.error(f"Error: Could not sort and compress BED file")
+        sys.exit(1)
+
+    def filter_vcf_by_bed(vcf_input, compressed_bed):
+        """
+        Filters variants using the BED file
+        Index the output vcf if gzipped
+        """
+
+        # Compute output filename based on input filename
+        in_path = Path(vcf_input)
+        out_path = output_dir / f"{in_path.stem}_filter_genes.vcf.gz"
+        out_format = "z" if out_path.suffix == ".gz" else "v"
+
+        # Bcftools command
+        cmd = [
+            "bcftools", "view",
+            "--regions-file", str(compressed_bed),
+            "--output", str(out_path),
+            "--output-type", out_format,
+            str(vcf_input)
+        ]
+
+        # Run the bcftools command
+        output = subprocess.run(cmd, capture_output=True, text=True)
+
+        if output.returncode != 0:
+            print("STDOUT:", output.stdout)
+            print("STDERR:", output.stderr)
+            sys.exit(output.returncode)
+
+        # Index the output VCF if gzipped
+        if out_path.suffix == ".gz":
+            index_cmd = [
+                "bcftools", "index", "-t", str(out_path)
+                ]
+            subprocess.run(index_cmd, capture_output=True, text=True)
+            
+        # Print output path so it can be captured by the pipeline
+        print(f"Filtered VCF written to {out_path}")
+
+        return out_path
+    
+    for vcf_file in vcf_input.split(","):
+
+        if not Path(vcf_file).is_file():
+            logging.error(f"Input VCF {vcf_file} does not exist")
+            sys.exit(1)
+        
+        output_vcf = filter_vcf_by_bed(vcf_file, compressed_bed)
+
+        if output_vcf:
+            logging.info(f"Processed input VCF {Path(vcf_file)} to {output_vcf.name}")
+        else:
+            logging.error(f"Failed to process input VCF {Path(vcf_file).name}")
+
+    logging.info(f"Successfully completed script '{script_name}'")
+
+    duration = time.time() - start_time
+    logging.info(f"Script '{script_name}' completed in {duration:.2f} seconds.")
+
+if __name__ == "__main__":
+    main()
